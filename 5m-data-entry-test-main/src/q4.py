9246a95097e165aafb17e495f7072a904d0fd81a
DarwinFD88 <darwinfd88@gmail.com>
2025-07-24 16:48:32 +0800

    q4.py

--- 5m-data-entry-test-main/src/q4.py
+++ 5m-data-entry-test-main/src/q4.py
@@ -1,4 +1,12 @@
 def string_reverse(s):
+    #First I want to check if input for 's' is a string
+    if type(s) == str:
+    #If 's' is indeed a string, then I want to return 's' in reverse by slicing that starts from the end of the string; moves backward by -1 step
+        return s[::-1]
+    else:
+    #If 's' is not a string; I want to print a message saying that 's' is not a string
+        print('This is not a string')
+        
     """
     Task 1
     - Create a function that reverses a given string (s).
@@ -12,3 +20,5 @@ def string_reverse(s):
 # Invoke the function "string_reverse" using the following scenarios:
 # - "Hello World"
 # - "Python"
+string_reverse("Hello World")
+string_reverse("Python")
